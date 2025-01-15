5d1e3d1a6f42d651cd35d156ffc0c50b526cd009
ThunderBlaze <darkthunderstormshiva@gmail.com>
2025-01-15 16:44:02 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+}
+

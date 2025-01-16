543a0d4519186837508f84b7b8789c514a7c855b
ThunderBlaze <darkthunderstormshiva@gmail.com>
2025-01-16 17:43:12 +0530

    Added All Components

---
+++ src/components/Settings.tsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+
+function Settings() {
+  return (
+    <div className='settings p-2 flex flex-col bg-slate-200 dark:bg-slate-800'>
+        <h1 className="text-2xl font-medium text-center">Settings</h1>
+    </div>
+  )
+}
+
+export default Settings
